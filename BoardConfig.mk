CyanogenMod/android_device_sony_yukon

init: use releasetools to set the firmware symlinks
All devices have a variant specific LTALabel partition. Mount it
and find the variant from the html file on this partition.

Thanks @invisiblek for showing me this exists
https://github.com/CyanogenMod/android_device_htc_m8-common/tree/cm-13.0/releasetools

Change-Id: I5c6db61db74bad79d27a16a0ee32e234bb733d08

BoardConfig.mk
@@ -114,6 +114,9 @@ TARGET_SYSTEM_PROP := device/sony/yukon/system.prop
 # NFC
 NFC_NXP_CHIP_TYPE := PN547C2
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/sony/yukon/releasetools
+
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 

LOS-flamingo/android_device_sony_flamingo

BoardConfig.mk
@@ -30,3 +30,6 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=flamingo
 
 # Vendor platform
 BOARD_VENDOR := sony
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/sony/flamingo/releasetools